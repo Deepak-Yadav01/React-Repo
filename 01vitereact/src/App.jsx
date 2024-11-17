0688c677f4077cafec9ea902055cabe240cc1bd1
Deepak Yadav <dspyadavdeepak01@gmail.com>
2024-11-17 19:38:33 +0530

    add customs files

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,11 @@ import Chai from "./chai"
 
 
 function App() {
-
+  const username = "chai aur code"
   return (
       <>
       <Chai/>
-      <h1>HELLO WORLD!</h1>
+      <h1>HELLO WORLD! {username}</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum temporibus suscipit sint hic sunt fugiat ab magnam, molestiae impedit soluta omnis nulla assumenda voluptas inventore labore officia quos ut magni?</p>
       </>
   )
