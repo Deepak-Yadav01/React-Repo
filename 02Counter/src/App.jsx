85ab1d79b671451a4a5cfc80e4360e742ac08b2f
Deepak Yadav <dspyadavdeepak01@gmail.com>
2024-11-18 22:31:02 +0530

    Adding maxCount and min value to the button , diseabled  prop on remove button

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -7,30 +7,46 @@ function App() {
 
   let [Counter, setcounter] = useState(0)  // () ke ander  koi bhivalue de skte h
 
+  const maxCount = 20; // Maximum value for the counter
+
   // let Counter = 10
 
   const addvalue = () => {
-    // console.log("value  Added", Math.random());
-    console.log("value Added", Counter);
-    Counter = Counter + 1
-    setcounter(Counter)
+    if (Counter < maxCount) {
+      // console.log("value  Added", Math.random());
+   
+    // Counter = Counter + 1
+    setcounter(Counter + 1)
+     console.log("value Added", Counter + 1);
+    }
+    
   }
 
 const removevalue = () => {
-  setcounter(Counter - 1)
+  if (Counter > 0) {
+    setcounter(Counter - 1)
+  }
+
 }
 
   return (
     <>
     <h1>Chai aur React</h1>
     <h2>Counter Value: {Counter}</h2>
+
+     {/* Add button: Will be disabled if counter reaches maxCount */}
+
     <button
-    onClick={addvalue}
+    onClick={addvalue} disabled={Counter >= maxCount}
     >Add Value {Counter}</button>
     <br />
+
+    {/* Remove button: Will be disabled if counter is 0 */}
+
     <button
-    onClick={removevalue}
+    onClick={removevalue} disabled={Counter <= 0}
     >Remove Value {Counter}</button>
+    
     <p>footer : {Counter}</p>
     </>
   )
